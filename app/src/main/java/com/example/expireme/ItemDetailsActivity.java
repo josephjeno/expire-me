6b6f3194f90b30887571736bc1555308f8e4d048
Joseph Jeno <joseph.jeno@gmail.com>
2019-07-18 10:02:24 -0400

    Item Details back button working

--- app/src/main/java/com/example/expireme/ItemDetailsActivity.java
+++ app/src/main/java/com/example/expireme/ItemDetailsActivity.java
@@ -2,13 +2,27 @@ package com.example.expireme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ItemDetailsActivity extends AppCompatActivity {
+
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
+
+        backButton = findViewById(R.id.add_item_button_back);
+    }
+
+    // When back button clicked
+    public void onbackButtonClicked(View view) {
+        Intent explicitIntent = new Intent(getApplicationContext(), ItemListActivity.class);
+        startActivity(explicitIntent);
     }
 }

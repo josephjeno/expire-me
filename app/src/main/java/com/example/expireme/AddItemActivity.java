50c68999c7121959d4b77c01562e0adb2d46aed6
Shlomi Yaakobovich <yaakobovich.a@husky.neu.edu>
2019-07-23 19:00:55 -0400

    back button on application top bar

--- app/src/main/java/com/example/expireme/AddItemActivity.java
+++ app/src/main/java/com/example/expireme/AddItemActivity.java
@@ -6,6 +6,8 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -54,9 +56,25 @@ public class AddItemActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+
+    @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         itemTitleTextView = findViewById(R.id.item_title_text);
         itemNameTextView = findViewById(R.id.add_item_name);
